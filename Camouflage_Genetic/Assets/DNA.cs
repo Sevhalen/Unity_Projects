2bd0a765dc2f883e26722239d1f55939bf8e702b
Sevhalen <deville.jb@gmail.com>
2018-07-26 18:43:03 +0200

    Creation from SPF

---
+++ Camouflage_Genetic/Assets/DNA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+	// Gene for color
+	public float r;
+	public float g;
+	public float b;
+
+	bool dead = false;
+
+	public float timeToDie = 0;
+
+	SpriteRenderer sRenderer;
+	Collider2D sCollider;
+
+	// Action lors d'un click sur un personnage
+	void OnMouseDown()
+	{
+		dead = true;
+		timeToDie = PopulationManager.elapsed;
+		//Debug.Log("Dead at : " + timeToDie);
+		sRenderer.enabled = false; // Fait disparaitre le personnage
+		sCollider.enabled = false; // Rend le pesonnage non-clickable
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Initialise les informations correspondant
+		// au personnage concerné
+		sRenderer = GetComponent<SpriteRenderer>();
+		sCollider = GetComponent<Collider2D>();
+		sRenderer.color = new Color(r,g,b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Camouflage_Genetic/Assets/PopulationManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour {
+
+	public GameObject personPrefab;
+	public int populationSize = 10;
+	List<GameObject> population = new List<GameObject>();
+	public static float elapsed = 0;
+	int trialTime = 10;
+	int generation = 1;
+
+	// Affichage d'informations à l'écran
+	GUIStyle guiStyle = new GUIStyle();
+	void OnGUI()
+	{
+		guiStyle.fontSize = 50;
+		guiStyle.normal.textColor = Color.white;
+		GUI.Label(new Rect(10, 10, 100, 20), "Generation : " + generation, guiStyle);
+		GUI.Label(new Rect(10, 65, 100, 20), "Trial Time : " + (int)elapsed, guiStyle);
+	}
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < populationSize; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(-9,9),Random.Range(-4.5f,4.5f),0);
+			GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+			go.GetComponent<DNA>().r = Random.Range(0.0f,1.0f);
+			go.GetComponent<DNA>().g = Random.Range(0.0f,1.0f);
+			go.GetComponent<DNA>().b = Random.Range(0.0f,1.0f);
+			population.Add(go);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if(elapsed > trialTime)
+		{
+			BreedNewPopulation();
+			elapsed = 0;
+		}
+		
+	}
+}
