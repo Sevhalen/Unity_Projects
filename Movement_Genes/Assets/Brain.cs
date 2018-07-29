df58da6b867cc0a27f9e92c0e95b0febf3c948df
Sevhalen <deville.jb@gmail.com>
2018-07-29 21:04:01 +0200

    End of Movement with Genes Part 1 from SPP

---
+++ Movement_Genes/Assets/Brain.cs
@@ -0,0 +1,81 @@
+﻿// Brain remplace le contrôle des personnages dans le jeu
+// Réalise l'interface entre l'ADN et le personnage
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson; // Donne accès aux contrôles du personnage
+
+[RequireComponent(typeof (ThirdPersonCharacter))] // Assurance de la présence d'un personnage
+public class Brain : MonoBehaviour {
+
+	public int DNALength = 1; // Fixation sur 1 gene pour montrer un exemple
+	public float timeAlive;
+	public DNA dna;
+
+	// Récupération du personnage
+	private ThirdPersonCharacter m_Character;
+	private Vector3 m_Move;
+	private bool m_Jump;
+	bool alive = true;
+
+	// Méthode pour "tuer" le personnage s'il touche un objet
+	// indiqué comme "dead"
+	// Typiquement, le sol blanc dans l'exemple dispose d'un tag "dead"
+	void OnCollisionEnter(Collision obj)
+	{
+		if(obj.gameObject.tag == "dead")
+		{
+			alive = false;
+		}
+	}
+
+	// Méthode d'initiamlisation s'executant lors de l'instanciation
+	// d'un prefab
+	public void Init()
+	{
+		// Initialisation de l'ADN
+		// 0 forward
+		// 1 back
+		// 2 left
+		// 3 right
+		// 4 jump
+		// 5 crouch
+		dna = new DNA(DNALength,6);
+		m_Character = GetComponent<ThirdPersonCharacter>();
+		timeAlive = 0;
+		alive = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Remplacement des mouvements du personnage par des mouvements commandés
+	// par le code
+	private void FixedUpdate()
+	{
+		// Lecture de l'ADN
+		float h = 0;
+		float v = 0;
+		bool crouch = false;
+		if(dna.GetGene(0) == 0) v = 1;
+		else if(dna.GetGene(0) == 1) v = -1;
+		else if(dna.GetGene(0) == 2) h = -1;
+		else if(dna.GetGene(0) == 3) h = 1;
+		else if(dna.GetGene(0) == 4) m_Jump = true;
+		else if(dna.GetGene(0) == 5) crouch = true;
+
+		m_Move = v*Vector3.forward + h*Vector3.right;
+		m_Character.Move(m_Move, crouch, m_Jump);
+		m_Jump = false;
+		if(alive)
+			timeAlive += Time.deltaTime;
+	}
+}
---
+++ Movement_Genes/Assets/DNA.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA {
+
+	List<int> genes = new List<int>();
+	int dnaLength = 0;
+	int maxValues = 0;
+
+	// Initialisation d'un gene
+	public DNA(int l, int v)
+	{
+		dnaLength = l;
+		maxValues = v;
+		SetRandom();
+	}
+
+	public void SetRandom()
+	{
+		genes.Clear(); // Vide la liste de genes
+		for(int i = 0; i < dnaLength; i++)
+		{
+			genes.Add(Random.Range(0, maxValues));
+		}
+	}
+
+	public void SetInt(int pos, int value)
+	{
+		genes[pos] = value;
+	}
+
+	// Méthode de combinaison des ADN des parents :
+	// la moitié provenant d'un parent, l'autre de l'autre parent
+	public void Combine(DNA d1, DNA d2)
+	{
+		for(int i = 0; i < dnaLength; i++)
+		{
+			if(i < dnaLength/2.0)
+			{
+				int c = d1.genes[i];
+				genes[i] = c;
+			}
+			else
+			{
+				int c = d2.genes[i];
+				genes[i] = c;
+			}
+		}
+	}
+
+	// Méthode de mutation : attribution d'une valeur aléatoire
+	// sur un gene aléatoire
+	public void Mutate()
+	{
+		genes[Random.Range(0,dnaLength)] = Random.Range(0, maxValues);
+	}
+
+	public int GetGene(int pos)
+	{
+		return genes[pos];
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
